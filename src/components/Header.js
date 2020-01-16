8d2f55391a9a161b8a4897fa9c1aa1543c247ca9
akpante3 <obije.victor5@gmail.com>
2020-01-16 23:33:51 +0100

    header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/stylesheets/header.css'
+
+
+function Header() {
+  return (
+    <div className="header">
+      <header className="App-header">
+  
+      </header>
+    </div>
+  );
+}
+
+export default Header;
