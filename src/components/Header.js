5f53af0f8db4b43004926d14f9f7ed0a5ece6958
akpante3 <obije.victor5@gmail.com>
2020-01-19 21:24:14 +0100

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import ham from '../assets/images/ham.svg'
 import '../assets/stylesheets/header.css'
 
 
 function Header() {
   return (
     <div className="header">
-      <header className="App-header">
-  
-      </header>
+      <div>
+        <img src={ham} className="header-img-burger" />
+      </div>
+      <div>
+        <img src='' />
+      </div>
     </div>
   );
 }
